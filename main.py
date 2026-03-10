498f01278d7d92399030f7b198215cac9d04673a
frango1401 <Franciscodemarchi8@gmail.com>
2026-03-10 11:41:08 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+a = int(input("digite um numero inteiro"))
+b = int(input("digite um numero inteiro"))
+c = float(input("digite um numero real"))
+
+mn = (a * 2) + (1/2)
+nm = (a * 3) + c
+mm = c ** 3
+
+print(f'o dobro do primeiro com metade do segundo: {mn}, o triplo do primeiro com o terceiro:{nm}, o terceiro elevado ao cubo {mm}')
